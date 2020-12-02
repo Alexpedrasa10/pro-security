f0d2196a69ec6d3377caa30ac12fe69441abad1d
Alex Pedrasa <32182113+Alexpedrasa10@users.noreply.github.com>
2020-12-02 00:21:38 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//Routes
+import { HomeComponent } from './components/home/home.component';
+import { ObjetivosComponent } from './components/objetivos/objetivos.component';
+import { PreguntasFrecuentesComponent } from './components/preguntas-frecuentes/preguntas-frecuentes.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'objetivos', component: ObjetivosComponent },
+  { path: 'preguntas-frecuntes', component: PreguntasFrecuentesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ObjetivosComponent } from './components/objetivos/objetivos.component';
+import { PreguntasFrecuentesComponent } from './components/preguntas-frecuentes/preguntas-frecuentes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ObjetivosComponent,
+    PreguntasFrecuentesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
